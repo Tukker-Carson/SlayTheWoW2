42a6bb71ffa0e43c4f3c25e3e8fb742005fa80a3
Carson Tukker <carson@tukker.us>
2026-07-11 13:29:00 -0500

    Holy Word: Chastise

---
+++ WoWTheSpireCode/Cards/Priest/Common/HolyWordChastise.cs
@@ -0,0 +1,26 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using WoWTheSpire.WoWTheSpireCode.CustomProperties;
+
+namespace WoWTheSpire.WoWTheSpireCode.Cards.Priest.Common;
+
+public class HolyWordChastise() : PriestCard(1, CardType.Attack, CardRarity.Common, TargetType.AnyEnemy) {
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [WoWKeywords.Holy];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(6, ValueProp.Move)];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play) {
+        ArgumentNullException.ThrowIfNull(play.Target, "cardPlay.Target");
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue).FromCard(this).Targeting(play.Target).WithHitFx("vfx/vfx_attack_slash").Execute(choiceContext);
+    }
+
+    public override async Task AfterCardPlayed(PlayerChoiceContext choiceContext, CardPlay cardPlay) {
+        if (!cardPlay.Card.Keywords.Contains(WoWKeywords.Holy) || Pile != null && Pile.Type != PileType.Hand) return;
+        await CardCmd.AutoPlay(choiceContext, this, null);
+        await CardPileCmd.Draw(choiceContext, Owner);
+    }
+
+    protected override void OnUpgrade() => DynamicVars.Damage.UpgradeValueBy(3);
+}
