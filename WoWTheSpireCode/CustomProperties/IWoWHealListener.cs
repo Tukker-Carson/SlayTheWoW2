5a09fde66e5c1ec3b0d69a240045469cdd1acff3
Carson Tukker <carson@tukker.us>
2026-07-26 21:06:24 -0500

    EchoOfLight

--- WoWTheSpireCode/CustomProperties/IWoWHealListener.cs
+++ WoWTheSpireCode/CustomProperties/IWoWHealListener.cs
@@ -14,9 +14,9 @@ public interface IWoWHealListener {
     /// <summary>Return the amount to multiply by.</summary>
     Decimal ModifyHealMultiplicative(Creature target, Creature source, Decimal amount, ValueProp props, CardPlay? cardPlay) => 1M;
     
-    /// <summary>Runs Before Heal but Amount is after Healing Buffs</summary>
+    /// <summary>Runs Before Heal and Amount Calculation</summary>
     Task AfterHealCalculated(Creature target, Creature source, Decimal amount, ValueProp props, CardPlay? cardPlay) => Task.CompletedTask;
     
-    /// <summary>Runs After Heal</summary>
+    /// <summary>Runs After Heal With Heal Amount</summary>
     Task AfterHeal(Creature target, Creature source, Decimal amount, ValueProp props, CardPlay? cardPlay) => Task.CompletedTask;
 }
--- WoWTheSpireCode/Powers/Priest/EchoOfLightPower.cs
+++ WoWTheSpireCode/Powers/Priest/EchoOfLightPower.cs
@@ -1,18 +1,20 @@
-﻿using BaseLib.Hooks;
+﻿using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Powers;
-using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using WoWTheSpire.WoWTheSpireCode.CustomProperties;
 
 namespace WoWTheSpire.WoWTheSpireCode.Powers.Priest;
 
-public sealed class EchoOfLightPower: WoWTheSpirePower, IHealAmountModifier {
+public sealed class EchoOfLightPower: WoWTheSpirePower, IWoWHealListener {
     public override PowerType Type => PowerType.Buff;
     public override PowerStackType StackType => PowerStackType.Single;
 
-    protected override IEnumerable<DynamicVar> CanonicalVars => [
-        new("heal1", 0),
-        new("heal2", 0),
-        new("heal3", 0)
-    ];
+    public Task AfterHealCalculated(Creature target, Creature source, Decimal amount, ValueProp props, CardPlay? cardPlay) {
+        if (target == Owner && target.HasPower<RenewPower>()) 
+            target.GetPower<RenewPower>()!.DynamicVars.Heal.BaseValue = Math.Max(target.GetPower<RenewPower>()!.DynamicVars.Heal.BaseValue, amount);
+        return Task.CompletedTask;
+    }
     
     
 }
