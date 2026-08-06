416cca721aa979728cfc9fb6c3596a3d56161429
Carson Tukker <carson@tukker.us>
2026-08-05 19:30:49 -0500

    Psyfiend

---
+++ WoWTheSpireCode/Cards/Priest/Rare/Psyfiend.cs
@@ -0,0 +1,26 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using WoWTheSpire.WoWTheSpireCode.Powers.Priest;
+
+namespace WoWTheSpire.WoWTheSpireCode.Cards.Priest.Rare;
+
+public class Psyfiend() : PriestCard(1, CardType.Power, CardRarity.Rare, TargetType.Self) {
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new PowerVar<PainSuppressionPower>(3),
+        new ("BasePowerLevel", 3)
+    ];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play) {
+        await PowerCmd.Apply<PsyfiendPower>(
+            new ThrowingPlayerChoiceContext(),
+            Owner.Creature,
+            DynamicVars[nameof(PainSuppressionPower)].BaseValue,
+            Owner.Creature,
+            this);
+    }
+
+    protected override void OnUpgrade() => EnergyCost.UpgradeBy(-1);
+}
---
+++ WoWTheSpireCode/Powers/Priest/PsyfiendPower.cs
@@ -0,0 +1,19 @@
+﻿using BaseLib.Extensions;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+
+namespace WoWTheSpire.WoWTheSpireCode.Powers.Priest;
+
+public class PsyfiendPower() : WoWTheSpirePower {
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    public override Task AfterSideTurnStart(CombatSide side, IReadOnlyList<Creature> participants, ICombatState combatState) {
+        if (!participants.Contains(Owner)) return Task.CompletedTask;
+        var target = CombatState.Enemies.Where(e => !e.IsDead && e.HasPower<FearPower>())
+            .MaxBy(e => e.GetPower<FearPower>()!.GetDynamicVar("Potency"));
+        if (target is not null) target.GetPower<FearPower>()!.Amount *= Amount+1;
+        return Task.CompletedTask;
+    }
+}
