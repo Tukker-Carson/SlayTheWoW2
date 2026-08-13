7105a70381b31b32d7e3c778ff39346f75185849
Carson Tukker <carson@tukker.us>
2026-08-13 17:09:40 -0500

    Spectral Guise

---
+++ WoWTheSpireCode/Cards/Priest/Uncommon/SpectralGuise.cs
@@ -0,0 +1,23 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using WoWTheSpire.WoWTheSpireCode.Powers.Priest;
+
+namespace WoWTheSpire.WoWTheSpireCode.Cards.Priest.Uncommon;
+
+public class SpectralGuise() : PriestCard(1, CardType.Skill, CardRarity.Uncommon, TargetType.Self) {
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<SpectralGuisePower>(1)];
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<FearPower>()];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay) {
+        await PowerCmd.Apply<SpectralGuisePower>(new ThrowingPlayerChoiceContext(), 
+            Owner.Creature,
+            DynamicVars[nameof(SpectralGuisePower)].BaseValue,
+            Owner.Creature,
+            null);
+    }
+    
+    protected override void OnUpgrade() => DynamicVars[nameof(SpectralGuisePower)].UpgradeValueBy(1);
+}
---
+++ WoWTheSpireCode/Powers/Priest/SpectralGuisePower.cs
@@ -0,0 +1,27 @@
+﻿using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace WoWTheSpire.WoWTheSpireCode.Powers.Priest;
+
+public class SpectralGuisePower: WoWTheSpirePower {
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    public override async Task AfterDamageReceived(PlayerChoiceContext choiceContext, Creature target, DamageResult result, ValueProp props,
+        Creature? dealer, CardModel? cardSource) {
+        if (target != Owner || result.UnblockedDamage == 0 || dealer is null) return;
+        await PowerCmd.Apply<FearPower>(choiceContext, dealer, 1, Owner, null);
+        dealer.GetPower<FearPower>()!.DynamicVars["Potency"].BaseValue = 
+            Math.Max(result.UnblockedDamage/2m, dealer.GetPower<FearPower>()!.DynamicVars["Potency"].BaseValue);
+        await PowerCmd.Decrement(this);
+    }
+
+    public override async Task AfterSideTurnStart(CombatSide side, IReadOnlyList<Creature> participants, ICombatState combatState) {
+        if (participants.Contains(Owner)) await PowerCmd.Remove(this);
+    }
+}
